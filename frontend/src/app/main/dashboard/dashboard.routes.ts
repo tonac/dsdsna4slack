ba394ae8ef023cffcf47c52a62d77cf0546bb816
tonac <antonio.sostar@ag04.com>
2017-11-24 09:22:22 +0100

    Added mocked graph visualized with visJS

--- frontend/src/app/main/dashboard/dashboard.routes.ts
+++ frontend/src/app/main/dashboard/dashboard.routes.ts
@@ -2,13 +2,15 @@ import { Route } from '@angular/router';
 
 import { DashboardComponent } from './dashboard.component';
 import { ArchiveRoutes } from './archive/archive.routes';
+import {ResultsRoutes} from './results/results.routes';
 
 export const DashboardRoutes: Route[] = [
     {
       path: 'dashboard',
       component: DashboardComponent,
       children: [
-        ...ArchiveRoutes
+        ...ArchiveRoutes,
+        ...ResultsRoutes,
       ]
     }
 ];
