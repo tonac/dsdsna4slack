b6689425af5c83fdaa6d7b7b529650a6070dfed4
jasna.jankovic <jasna.jankovic@fer.hr>
2017-11-27 17:40:01 +0100

    revision2

--- backend/archives/permissions.py
+++ backend/archives/permissions.py
@@ -1,7 +1,8 @@
 from rest_framework import permissions
-from archives.models import Archive
-
+from archives.models import Archive, SlackUser, Channel, Message
 
 class IsArchiveOwner(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
-        return isinstance(obj, Archive) and obj.user == request.user
+        return (isinstance(obj, Archive) and obj.user == request.user) or \
+                ((isinstance(obj, SlackUser) or isinstance(obj, Channel)) and obj.archive.user == request.user) or \
+                (isinstance(obj, Message) and obj.channel.archive.user == request.user)
--- backend/archives/serializers.py
+++ backend/archives/serializers.py
@@ -1,82 +1,32 @@
 from rest_framework import serializers
 from archives.models import Archive, SlackUser, Channel, Message
 
-class SlackUserSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = SlackUser
-        fields = ('id', 'slack_id', 'name')
-
-class ChannelSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Channel
-        fields = ('id', 'channel_id', 'name')
-
-class ArchiveSerializer(serializers.ModelSerializer):
-    slackusers = SlackUserSerializer(many=True, required=False)
-    channels = ChannelSerializer(many=True, required=False)
-
+class ArchiveSerializer(serializers.HyperlinkedModelSerializer):
+    slackusers = serializers.HyperlinkedRelatedField(read_only=True, view_name='slackuser-detail', many=True)
+    channels = serializers.HyperlinkedRelatedField(read_only=True, view_name='channel-detail', many=True)
     class Meta:
         model = Archive
-        fields = ('name', 'uploaded', 'slackusers', 'channels')
+        fields = ('url', 'name', 'uploaded', 'slackusers', 'channels')
 
-class FileSerializer(serializers.Serializer):
-    file = serializers.FileField()
+class SlackUserSerializer(serializers.HyperlinkedModelSerializer):
+    archive = serializers.HyperlinkedRelatedField(read_only=True, view_name='archive-detail')
+    class Meta:
+        model = SlackUser
+        fields = ('url', 'slack_id', 'name', 'archive')
 
-    def create(self, validated_data):
-        slack_archive = zipfile.ZipFile(file_obj) 
-        #slack_archive = zipfile.ZipFile('zip3.zip')
-        # check if zip file is valid
-        if slack_archive.testzip() is None:
-            # search for users.json and channels.json
-            archive_files = slack_archive.namelist()
-            if 'users.json' in archive_files and 'channels.json' in archive_files:
-                # save archive
-                #archive_serializer = ArchiveSerializer(data={'name': filename}, context={'request': request})   #
-                archive_serializer = ArchiveSerializer(data={'name': slack_archive.filename}, context={'request': request})
-                archive_serializer.is_valid(raise_exception=True)
-                archive = archive_serializer.save(user=request.user)
-                # get users
-                users_info = slack_archive.getinfo('users.json')
-                users_file = slack_archive.open(users_info)
-                users_list = json.load(users_file)
-                users_file.close()
-                # save users
-                for user in users_list:
-                    slack_user_serializer = SlackUserUploadSerializer(
-                        data={'user_data': user})
-                    slack_user_serializer.is_valid(raise_exception=True)
-                    slack_user_serializer.save(archive=archive)
-                # get channels
-                channels_info = slack_archive.getinfo('channels.json')
-                channels_file = slack_archive.open(channels_info)
-                channels_list = json.load(channels_file)
-                channels_file.close()
-                # save channels
-                for channel in channels_list:
-                    channel_serializer = ChannelUploadSerializer(
-                        data={'channel_data': channel})
-                    channel_serializer.is_valid(raise_exception=True)
-                    channel_serializer.save(archive=archive)
-                # get messages
-                for fileinfo in slack_archive.infolist():
-                    # if channel file
-                    if fileinfo.file_size > 0:
-                        path = fileinfo.filename.split("/")
-                        if len(path) > 1:
-                            channel_name = path[0]
-                            channel = Channel.objects.get(
-                                archive=archive, name=channel_name)
-                            messages_file = slack_archive.open(fileinfo)
-                            messages_list = json.load(messages_file)
-                            messages_file.close()
-                            for msg in messages_list:
-                                message_serializer = MessageUploadSerializer(
-                                    data={'message_data': msg})
-                                message_serializer.is_valid(
-                                    raise_exception=True)
-                                message_serializer.save(channel=channel)
+class ChannelSerializer(serializers.HyperlinkedModelSerializer):
+    archive = serializers.HyperlinkedRelatedField(read_only=True, view_name='archive-detail')
+    messages = serializers.HyperlinkedRelatedField(read_only=True, view_name='message-detail', many=True)
+    class Meta:
+        model = Channel
+        fields = ('url', 'channel_id', 'name', 'messages','archive')
+
+class MessageSerializer(serializers.HyperlinkedModelSerializer):
+    channel = serializers.HyperlinkedRelatedField(read_only=True, view_name='channel-detail')
+    slackuser = serializers.HyperlinkedRelatedField(read_only=True, view_name='slackuser-detail')
+    class Meta:
+        model = Message
+        fields = ('url', 'slackuser', 'channel', 'text')
 
 class SlackUserUploadSerializer(serializers.Serializer):
     user_data = serializers.JSONField()
@@ -87,7 +37,6 @@ class SlackUserUploadSerializer(serializers.Serializer):
         validated_data = validated_data.pop('user_data')
         return SlackUser.objects.create(slack_id=validated_data['id'], team_id=validated_data['team_id'], name=validated_data['name'], archive=archive)
 
-
 class ChannelUploadSerializer(serializers.Serializer):
     channel_data = serializers.JSONField()
     archive = ArchiveSerializer(required=False)
@@ -95,8 +44,7 @@ class ChannelUploadSerializer(serializers.Serializer):
     def create(self, validated_data):
         archive = validated_data['archive']
         validated_data = validated_data.pop('channel_data')
-        return Channel.objects.create(channel_id=validated_data['id'], name=validated_data['name'], archive=archive)
-
+        return Channel.objects.create(channel_id= validated_data['id'], name=validated_data['name'], archive=archive)
 
 class MessageUploadSerializer(serializers.Serializer):
     message_data = serializers.JSONField()
@@ -108,6 +56,5 @@ class MessageUploadSerializer(serializers.Serializer):
         user = validated_data.get("user")
         slack_user = None
         if user is not None:
-            slack_user = SlackUser.objects.get(
-                archive=channel.archive, slack_id=user)
+            slack_user = SlackUser.objects.get(archive=channel.archive, slack_id=user)
         return Message.objects.create(slackuser=slack_user, channel=channel, text=validated_data['text'])
