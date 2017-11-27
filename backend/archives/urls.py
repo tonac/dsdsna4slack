0decb6332647edbb39da96a75504c04372ada500
jasna.jankovic <jasna.jankovic@fer.hr>
2017-11-27 18:44:29 +0100

    revision3

--- backend/archives/urls.py
+++ backend/archives/urls.py
@@ -1,8 +1,15 @@
 from django.conf.urls import url, include
 from backend import views
+from rest_framework import routers
 
-from archives.views import ArchiveViewSet
+from archives.views import ArchiveViewSet, ChannelViewSet, SlackUserViewSet
+
+router = routers.DefaultRouter()
+router.register(r'archives', ArchiveViewSet, base_name='archive')
+router.register(r'slackusers', SlackUserViewSet, base_name='slackuser')
+router.register(r'archives', ChannelViewSet, base_name='channel')
+
 
 urlpatterns = [
-    url(r'^', ArchiveViewSet.as_view()),
+    url(r'^', include(router.urls)),
 ]
