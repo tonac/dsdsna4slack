d94f5b9efbd0ba0b8aacf949da072ea577662fae
Marin Marjanović <marin_marjanovic@me.com>
2018-01-07 13:46:38 +0100

    Created UIConstants and changed what was requested.

---
+++ frontend/src/app/utils/UIConstants.ts
@@ -0,0 +1,9 @@
+export class UIConstants {
+
+    public densityExplanationForMentionBased: string = "The proportion of edges in a graph compared to all possible edges that could be in that graph.";
+    public densityExplanationForSubscriptionBased: string = "The proportion of edges in a graph compared to all possible edges from people to channels that could be in that graph.";
+    public pathLengthExplanation: string = "The average distance between any two persons in a graph.";
+    public edgeConnectivityExplanation: string = "A minimum number of edges you have to remove before you can even hope to disconnect the graph.";
+    public nodeConnectivityExplanation: string = "A minimum number of nodes you have to remove before you can even hope to disconnect the graph.";
+
+}
