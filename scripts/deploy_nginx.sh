f6e2535d1040d607a053298d75f916ff85b155e3
Ivan Lakovic <laky55555@gmail.com>
2018-01-08 21:32:01 +0100

    Small changes for deployment server

--- scripts/deploy_nginx.sh
+++ scripts/deploy_nginx.sh
@@ -6,10 +6,11 @@ cd ../nginx
 
 docker rm -f deploy_nginx &> /dev/null
 
-docker run --name=deploy_nginx --publish=80:80 \
+docker run --name=deploy_nginx --publish=80:80 --publish=443:443 \
               --volume=`pwd`/frontend:/app/frontend/:ro \
               --volume=`pwd`/conf.d:/etc/nginx/conf.d:ro \
               --volume=`pwd`/backend:/app/backend/:ro \
+              --volume=`pwd`/certs:/app/certs/:ro \
               --link deploy_django:django \
               -it -d ${image_name}
 
